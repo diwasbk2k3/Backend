0f34157753d6afc75a3b632d72408adb5c75e6f0
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-23 10:23:37 +0545

    Create Book

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,21 +1,43 @@
 import { Request, Response } from "express";
 
+export type Book = {
+    id: string,
+    title: string,
+    date?: string //Optional
+}
+
+const books: Book[] = [
+    {
+        id: "B-1",
+        title: "1984",
+        date: "2022-10-11"
+    },
+    {
+        id: "B-2",
+        title: "To Kill a Mocking Bird"
+    }
+]
 export class BookController {
+    createBook = (req: Request, res: Response) => {
+        const { id, title } = req.body //destructure
+        if (!id) {
+            return res.status(400).json({ message: "Book ID is required" })
+        }
+        if (!title) {
+            return res.status(400).json({ message: "Book title is required" })
+        }
+        const exist = books.find(book => book.id == id)
+        if (exist) {
+            return res.status(400).json({ message: "Book with this ID already exist" })
+        }
+
+        const newBook: Book = { id, title }
+        // Same as {id: id, title: title} // variable and key name same
+        books.push(newBook)
+        return res.status(201).json(newBook)
+
+    }
     getBooks = (req: Request, res: Response) => {
-        const books = [
-            {
-                id: "B-1",
-                title: "1984",
-                date: "2022-10-11"
-            },
-            {
-                id: "B-2",
-                title: "To Kill a Mocking Bird"
-            }
-        ]
-        res.send({
-            message: "Books",
-            result: books
-        })
+        res.status(200).json(books)
     }
 }
