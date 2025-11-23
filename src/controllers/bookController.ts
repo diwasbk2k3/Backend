c66c72a05fd8002e1ec2855b56eaf53d61ff1f6d
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-23 10:39:22 +0545

    Zod Validation

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
+import {z} from "zod"
 
-export type Book = {
-    id: string,
-    title: string,
-    date?: string //Optional
-}
+export const BookSchema = z.object({
+    id: z.string().min(1, "Book ID is required"),
+    title: z.string().min(1, "Book Title is required"),
+    date: z.string().optional() //Optional
+})
+
+export type Book = z.infer<typeof BookSchema> // automatically create type from schema
+// schema, more tham type checking - rutime validation
+// what define the shape of data, what validates data > book
+
+// export type Book = {
+//     id: string,
+//     title: string,
+//     date?: string //Optional
+// }
 
 const books: Book[] = [
     {
